5d03573e8091f9efb2aa9fcae98b40824c64f004
haassanbash <hassanbasheer135@gmail.com>
2026-06-28 15:43:04 +0300

    feet:added commisions and messages

--- src/app/[locale]/(control-panel)/messages/page.tsx
+++ src/app/[locale]/(control-panel)/messages/page.tsx
@@ -1,5 +1,16 @@
+import { getTranslations } from 'next-intl/server';
 import MessagesListView from 'src/sections/admin-dashboard/messages/messages-list-view';
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   return <MessagesListView />;
 }
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'Metadata.ContactUs' });
+
+  return {
+    title: t('title'),
+  };
+}
+
